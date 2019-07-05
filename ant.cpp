375d46ca1bef1ce21334b449602632c87a12a896
Lee <leestackcz@gmail.com>
2019-07-05 17:44:50 +0200

    somewhat functionl represantion of plane ..

--- ant.cpp
+++ ant.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+
 using namespace std;
 
 class ant{ 
+  vector<int> path;
 public: 
   virtual void moveNext() {};
 };
@@ -11,7 +15,8 @@ public:
 class plane{
 public:
   plane() {};
-  virtual vector<int> getAdjVal(int vertex) = 0;
+  // virtual vector<int> getAdjVal(int vertex) = 0;
+  virtual int getNextVertex(int vertex) = 0;
   virtual vector<int> getAdjPheromons(int vertex) = 0;
   virtual bool addAdjPheromon(int vertex) = 0;
   virtual void updatePheromons() = 0;
@@ -19,20 +24,47 @@ public:
 
 
 class my_plane : plane{
-  vector<tuple<int, int, int>> edge;
+  // vector<tuple<int, int, int>> edge;
+  map<int, vector<tuple<int, int>>> edges; //from , <to , value>
   vector<int> vertex;
-public:
-  my_plane(){
 
-  };
-
-  
-
-  vector<int> getAdjVal(int vertex) override {
+  vector<int> getAdjVal(int vertex){
     auto k = vector<int>();
     k.emplace_back(vertex);
     return k;
-  } 
+  }
+
+public:
+  my_plane(){
+    edges = map<int, vector<tuple<int, int>>>();
+  };
+
+  void insEdge2(int from , int to, int value){
+    if (from != to)
+    {
+      edges[from].emplace_back(make_tuple(to, value));
+      edges[to].emplace_back(make_tuple(from, value));
+    }
+  };
+
+  void WA(){
+    for( auto const& [key, val] : edges )
+    {
+      std::cout << key << ':';
+      for( auto const& [a, b] : val ) {
+        cout << "to: " << a << ", val :" << b << "|";
+      }      // string's value
+        cout << std::endl ;
+    }
+  };
+
+
+  int getNextVertex(int vertex) override {
+    return 7;
+  };
+
+
+ 
 
   vector<int> getAdjPheromons(int vertex) override {
     return vector<int>();
@@ -52,12 +84,27 @@ public:
 
 
 int main() {
-  std::string name;
-  std::cout << "What is your name? ";
-  getline (std::cin, name);
-  std::cout << "Hello, " << name << "!\n";
-
+  string file = "g1.in";
 
   auto mp = new my_plane();
-  cout << mp->getAdjVal(1)[0];
+  ifstream inpu(file);
+
+  if (!inpu)
+  { 
+    cout << "unable to open file";
+    return -1;
+  }
+  
+  int from, to, value;
+
+  while (inpu >> from >> to >> value)
+  {
+    mp->insEdge2(from, to, value);
+  }
+  
+  mp->WA();
+  
+
+  
+  // cout << mp->getAdjVal(1)[0];
 }
