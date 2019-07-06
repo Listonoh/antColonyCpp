f5736850f142bd89389d09c0c0479d4fe38a80f9
Lee_Stack <leestackcz@gmail.com>
2019-07-06 14:39:09 +0200

    base structure for tsp

--- ant.cpp
+++ ant.cpp
@@ -6,12 +6,6 @@
 
 using namespace std;
 
-class ant{ 
-  vector<int> path;
-public: 
-  virtual void moveNext() {};
-};
-
 class plane{
 public:
   plane() {};
@@ -26,7 +20,8 @@ public:
 class my_plane : plane{
   // vector<tuple<int, int, int>> edge;
   map<int, vector<tuple<int, int>>> edges; //from , <to , value>
-  vector<int> vertex;
+  map<tuple<int, int> , int> pheromones;
+  // vector<int> vertex;
 
   vector<int> getAdjVal(int vertex){
     auto k = vector<int>();
@@ -39,18 +34,23 @@ public:
     edges = map<int, vector<tuple<int, int>>>();
   };
 
+  void updatePheromons(vector<int>& path){
+    cout << "updating pheromones \n"
+  }
+
   void insEdge2(int from , int to, int value){
     if (from != to)
     {
       edges[from].emplace_back(make_tuple(to, value));
       edges[to].emplace_back(make_tuple(from, value));
+
     }
   };
 
   void WA(){
     for( auto const& [key, val] : edges )
     {
-      std::cout << key << ':  ';
+      std::cout << key << ":  ";
       for( auto const& [a, b] : val ) {
         cout << "to: " << a << ", val :" << b << " | ";
       }     
@@ -84,6 +84,60 @@ public:
 };
 
 
+
+class ant{ 
+  vector<int> path;
+public: 
+  virtual void moveNext() = 0;
+  virtual tuple<int, vector<int>> findPath() = 0;
+};
+
+class AntTSP : ant{
+  my_plane pl;
+  public:
+  AntTSP(my_plane& plan) {
+    pl = plan;
+  };
+
+  void moveNext() override {
+
+  };
+
+  tuple<int, vector<int>> findPath() override {
+    
+
+    // auto a = {1,2,3};
+    auto a = vector<int>{1,2,3};
+    cout << a[2];
+    return make_tuple(1,a);
+    // return make_tuple(1, make_vector(1,2,3));
+  };
+};
+
+bool isOptimal(int value, int iteration){
+  return true;
+}
+
+vector<int> AntColonyTSP(my_plane& plan, int n = 20){
+  auto ant1 = new AntTSP(plan);
+  // tuple<int, vector<int>> bestResult = ant1->findPath();
+  auto bestResult = ant1->findPath();
+  int iteration = 0;
+
+  while (!isOptimal(get<0>(bestResult), iteration)){
+    iteration ++;
+    for (size_t i = 0; i < n; i++){
+      delete(ant1);
+      ant1 = new AntTSP(plan);
+      auto result = ant1->findPath();
+      if (get<0>(result) <= get<0>(bestResult)) bestResult = result;
+    }
+    plan.updatePheromons(get<1>(bestResult));
+  }
+}
+
+
+
 int main() {
   string file = "g1.in";
 
@@ -102,8 +156,7 @@ int main() {
   {
     mp->insEdge2(from, to, value);
   }
-  impu.colose();
-  
+  //impu.close();
   mp->WA();
   
 
